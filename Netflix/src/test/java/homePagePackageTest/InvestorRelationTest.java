32a41d8d654f1c0d29b23f2b9c5aa8c1a68e3d04
WinsenSid <winsen.sid@gmail.com>
2019-05-21 16:16:41 -0400

    first commit

---
+++ Netflix/src/test/java/homePagePackageTest/InvestorRelationTest.java
@@ -0,0 +1,11 @@
+package homePagePackageTest;
+
+import homePagePackage.InvestorRelation;
+import org.testng.annotations.Test;
+
+public class InvestorRelationTest extends InvestorRelation {
+        @Test(priority = 1)
+        public void InvestorRelationBT(){
+        InvestorRelationButton();
+    }
+}
